723df63e71e28360e73e3b9dce89acc24cea64d3
Giselle Montero <gisnetz@msn.com>
2019-03-03 21:38:15 +0100

    Added header v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import React, { Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { getMentors } from './api/mentors';
-import Home from './components/Home';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { getMentors } from "./api/mentors";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 class App extends Component {
   componentDidMount() {
-    getMentors().then((modules) => {
+    getMentors().then(modules => {
       console.log(modules);
-    })
+    });
   }
 
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/:path(|index|home|start)" component={Home} />
+          <Route path="/:path(|index|home|start)" component={(Home, Header)} />
           <Route render={() => <p>Page not found</p>} />
         </Switch>
       </BrowserRouter>
