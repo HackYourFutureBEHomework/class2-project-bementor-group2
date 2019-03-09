097e1c0dff302e1456f9609998e72b3f41137946
Hassan Ali <hassanalihazaraa@gmail.com>
2019-03-09 22:30:27 +0100

    polished version lol

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Foto from "../assets/images/ticket.jpg";
+const Body = () => {
+  return (
+    <div>
+      <p>
+        Be Mentor is an organistion that Lorem ipsum dolor sit amet, ante aenean
+        pellentesque at aliquet. Maecenas accumsan, dui purus, morbi aenean sed
+        risus dictum amet est, quis tincidunt, natoque gravida venenatis dis.
+        Aenean ac nunc, morbi neque. Est aliquet in erat interdum euismod hac,
+        fringilla pellentesque erat eleifend integer magna condimentum, placerat
+        quam quod. Et risus placerat proin eros, nunc sed quisque ac donec,
+        pellentesque enim lectus quam quis morbi et <br />
+        <img src={Foto} alt="random photo" width="25%" />
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default Body;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../assets/css/Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header_image">
+        <h1 className="header_title">BE MENTOR</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="#Connect">Connect</Link>
+          </li>
+          <li>
+            <Link to="#Contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="#Login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
