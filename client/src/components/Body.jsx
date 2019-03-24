433f4ca81beaf51fe85c7f380fe5f9cb7564ea90
Giselle Montero <gisnetz@msn.com>
2019-03-24 01:32:55 +0100

    Added full detail for user search result

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Search from "../components/Search";
-import SearchDetail from "../components/SearchDetail";
+import SearchUserDetail from "../components/SearchUserDetail";
 import User from "../components/User";
 
 import "../assets/css/Body.css";
@@ -12,7 +12,7 @@ const Body = () => {
     <div className="body">
       <Switch>
         <Route path="/:path(|index|home|start)" component={Home} />
-        <Route path="/search/users/:userId" component={SearchDetail} />
+        <Route path="/search/users/:userId" component={SearchUserDetail} />
         <Route path="/:path(|search|find)" component={Search} />
         <Route path="/user" component={User} />
         <Route render={() => <p>Page not found</p>} />
