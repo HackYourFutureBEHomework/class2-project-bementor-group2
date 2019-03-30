07617a408c8b2f261afc4b5eb37941c8e1c1b247
Giselle Montero <gisnetz@msn.com>
2019-03-30 22:41:50 +0100

    Fix case for Index.css reference

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import "normalize.css";
-import "./assets/css/index.css";
+import "./assets/css/Index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
